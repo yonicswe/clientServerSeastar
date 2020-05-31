46e73b0dfe439f93fbead66059eeac915bb633c8
Yonatan Cohen <yonatanc@mellanox.com>
2020-05-31 15:00:06 +0300

    Added histogram

--- client.cpp
+++ client.cpp
@@ -1,123 +1,120 @@
-#include <vector>
+#include <ctime>
+#include <array>
+#include <chrono>
+#include <random>
 #include <numeric>
+#include <cstdlib>
 #include <iostream>
 #include <algorithm>
+#include <filesystem>
 #include <type_traits>
-#include <seastar/core/gate.hh>
-#include <seastar/core/app-template.hh>
+#include <seastar/core/align.hh>
+#include <seastar/core/sleep.hh>
+#include <seastar/core/print.hh>
+#include <seastar/core/sstring.hh>
+#include <seastar/core/thread.hh>
 #include <seastar/core/reactor.hh>
+#include <seastar/core/fstream.hh>
 #include <seastar/core/future-util.hh>
 #include <seastar/core/distributed.hh>
+#include <seastar/core/app-template.hh>
+
+#include "common.h"
+
+namespace cs = seastar;
+
+cs::logger lg("client");
+
+std::random_device randdev;
+std::default_random_engine randeng(randdev()); //replace randdev with hardcoded number to get same sequence
+std::uniform_int_distribution<uint8_t> sleep_between_msgs_dist(0, 10);
 
 
 namespace bpo = boost::program_options;
 
 class client;
-seastar::distributed<client> clients;
+cs::distributed<client> clients;
 
-seastar::logger lg("client");
-
-
-namespace utils{
-
-template<typename TContainer>
-std::ostream& print_histogram(std::ostream& out, std::string header, const TContainer& container) {
-	static_assert(std::is_unsigned<typename TContainer::value_type>::value);
-	out << header;
-	auto max_value = *std::max_element(container.begin(), container.end());
-	if (!max_value){
-		return out << std::endl << "  bin[*] = 0";
-	}
-
-	typename TContainer::value_type seen_so_far = {};
-	auto total = std::accumulate(container.begin(), container.end(), 0);
-
-	for(size_t bin_idx = 0; bin_idx < container.size(); ++bin_idx){
-		if(container[bin_idx]){
-			seen_so_far += container[bin_idx];
-			const uint16_t percentage = 100*(long double)(seen_so_far)/(long double)(total);
-			out << std::endl << "  bin[" << bin_idx << "] = " << container[bin_idx] << " " << percentage << "%";
-		}
-	}
-	return out;
-}
-}
 
 struct statistics{
-	statistics()
-		: latency(32, 0)
-	{}
+	typedef std::array<uint64_t, 2048> latency_hist_t;
 
-	friend std::ostream& operator<<(std::ostream& out, const statistics& stats){
-		auto elapsed = stats.finished - stats.started;
-		auto usecs = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
-		auto secs = static_cast<double>(usecs) / static_cast<double>(1000 * 1000);
-		auto requests_per_seconds = static_cast<double>(stats.n_requests)/secs;
+	cs::sstring format_general() const{
+		auto duration_us = std::max(1L, std::chrono::duration_cast<std::chrono::microseconds>(_finished - _started).count());
+		//auto duration_s = std::max(1.0, static_cast<double>(duration_us) / static_cast<double>(1000 * 1000));
+		//auto throughput = static_cast<double>(_n_requests)/duration_s;
+		//auto bandwidth = static_cast<double>(_bytes_written+_bytes_read)/duration_s;
 
-		out << "duration=" << usecs << " micro, "
-			<< "bytes(w=" << stats.bytes_written << ", r=" << stats.bytes_read << "), "
-			<< "requests=(total=" << stats.n_requests << ", rate=" << requests_per_seconds << "), "
-			<< "responses=" << stats.n_responses 
-			<< std::endl;
-
-		utils::print_histogram(out, "request/response latency (microseconds)", stats.latency);
-
-		return out;
+		return cs::format( "bytes_written,bytes_read,n_requests,n_responses,duration_us\r\n"
+						   "{},{},{},{},{}\r\n"
+						   , _bytes_written,_bytes_read,_n_requests,_n_responses,duration_us);
 	}
-
-	statistics& operator+=(const statistics& other){
-		bytes_read += other.bytes_read;
-		bytes_written += other.bytes_written;
-		n_requests += other.n_requests;
-		n_responses += other.n_responses;
-		started = std::min(started, other.started);
-		finished = std::max(finished, other.finished);
-		for(size_t idx = 0; idx < latency.size(); ++idx){
-			latency[idx] += other.latency[idx];
+			
+	cs::sstring format_latency() const{
+		cs::sstring result;
+		result += cs::sstring("latency_us,messages\r\n");
+		for(size_t ltnc = 0; ltnc < _latency.size(); ++ltnc){
+			auto msgs = _latency[ltnc];
+			if(msgs){
+				result += cs::format("{},{}\r\n", ltnc, msgs);
+			}
 		}
-		return *this;
+		return result;
 	}
 
-	seastar::future<> freeze(){
-		finished = seastar::lowres_clock::now();
-		return seastar::make_ready_future<>();
+	cs::future<> freeze(){
+		_finished = cs::lowres_clock::now();
+		return cs::make_ready_future<>();
 	}
 
 	void write(uint64_t bytes){
-		bytes_written += bytes;
-		n_requests += 1;
+		_bytes_written += bytes;
+		_n_requests += 1;
 	}
 
 	void read(uint64_t bytes, std::chrono::microseconds rr_duration){
-		bytes_read += bytes;
-		n_responses += 1;
-		latency[std::min(latency.size() - 1, size_t(rr_duration.count()))] += 1;
+		_bytes_read += bytes;
+		_n_responses += 1;
+		_latency[std::min(_latency.size() - 1, size_t(rr_duration.count()))] += 1;
 	}
 
-	uint64_t bytes_read = 0;
-	uint64_t bytes_written = 0;
-	uint64_t n_requests = 0;
-	uint64_t n_responses = 0;
-	seastar::lowres_clock::time_point started = seastar::lowres_clock::now();
-	seastar::lowres_clock::time_point finished = seastar::lowres_clock::now();
-	std::vector<uint64_t> latency = std::vector<uint64_t>(32,0);
+private:
+	uint64_t _bytes_read = 0;
+	uint64_t _bytes_written = 0;
+	uint64_t _n_requests = 0;
+	uint64_t _n_responses = 0;
+	latency_hist_t _latency = {};
+	cs::lowres_clock::time_point _started = cs::lowres_clock::now();
+	cs::lowres_clock::time_point _finished = cs::lowres_clock::now();
 };
 
 
 struct connection {
-	explicit connection(size_t idx, seastar::connected_socket&& fd)
-	: _idx(idx)
+	struct configuration{
+		configuration change_id(size_t id) const {
+			auto cfg = *this;
+			cfg.id = id;
+			return cfg;
+		}
+
+		size_t id;
+		int64_t n_msgs;
+		bool sleep_between_msgs;
+	};
+
+	explicit connection(configuration cfg, cs::connected_socket&& fd)
+	: _cfg(cfg)
 	  , _fd(std::move(fd))
 	  , _read_buf(_fd.input())
 	  , _write_buf(_fd.output())
 	{}
 
-	seastar::future<> send(int64_t times){
+	cs::future<> send(){
 		_stats = statistics();
 
-		auto loop = seastar::do_for_each(boost::counting_iterator<int64_t>(0)
-										, boost::counting_iterator<int64_t>(times)
-										, [this](auto loop_idx){return send1(loop_idx);});
+		auto loop = cs::do_for_each(boost::counting_iterator<int64_t>(0)
+									, boost::counting_iterator<int64_t>(_cfg.n_msgs)
+									, [this](auto loop_idx){return send1(loop_idx);});
 
 		return loop.finally([this]{ return _stats.freeze(); });
 	}
@@ -126,121 +123,145 @@ struct connection {
 
 private:
 
-	seastar::future<> send1(int64_t loop_idx){
-		auto started = seastar::lowres_clock::now();
-		lg.debug("client connect_and_talk - connection({}) - loop({}) - writing data", _idx, loop_idx);
-		return _write_buf.write("ping")
-			   .then([this] {
-					return _write_buf.flush();
-				})
-			   .then([this, started, loop_idx] {
-					_stats.write(4);
-					lg.debug("client connect_and_talk - connection({}) - loop({}) - writing data - done", _idx, loop_idx);
-					return _read_buf.read_exactly(4)
-						   .then([this, started, loop_idx] (seastar::temporary_buffer<char> buf) {
-								lg.debug("client connect_and_talk - connection({}) - loop({}) - reading data - done({})", _idx, loop_idx, buf.size());
-								auto duration = std::chrono::microseconds(seastar::lowres_clock::now() - started);
-								_stats.read(buf.size(), duration);
-								if (buf.size() != 4) {
-									lg.error("client connect_and_talk - connection({}) - loop({}) - reading data - done({} != 4)", _idx, loop_idx, buf.size());
-									return seastar::make_ready_future<>();
-								}
-								auto str = std::string(buf.get(), buf.size());
-								if (str != "ping") {
-									lg.error("client connect_and_talk - connection({}) - loop({}) - reading data - done({} != ping)", _idx, loop_idx, str);
-									return seastar::make_ready_future<>();
-								}
-								return seastar::make_ready_future<>();
-							});
-				});
+	cs::future<> send1(int64_t loop_idx){
+		_write_msg = utils::make_message(loop_idx);
+		auto started = std::chrono::steady_clock::now();
+		auto written = utils::write_message(_write_buf, _write_msg);
+		auto read = written.then([this] { 
+			_stats.write(utils::get_io_message_size(_write_msg));
+			if (_cfg.sleep_between_msgs){
+				auto period = std::chrono::microseconds(sleep_between_msgs_dist(randeng));
+				return cs::sleep(period).then([this, period]{ return utils::read_message(_read_buf, _read_msg); });
+			} else {
+				return utils::read_message(_read_buf, _read_msg);
+			}
+		});
+		return read.then([this, started]{
+			auto finished = std::chrono::steady_clock::now();
+			auto duration_fine = std::chrono::nanoseconds(finished - started); //steady clock is using nanoseconds
+			auto duration = std::chrono::microseconds(duration_fine.count() / 1000);
+			_stats.read(utils::get_io_message_size(_read_msg), duration);
+			if (_read_msg.number != _write_msg.number){
+				lg.error("client connect_and_talk - connection({}) - read({}) != written({})", _cfg.id, _read_msg.number, _write_msg.number);
+			}
+			return cs::make_ready_future<>();
+		});
 	}
 
 private:
-	const size_t _idx;
-	seastar::connected_socket _fd;
-	seastar::input_stream<char> _read_buf;
-	seastar::output_stream<char> _write_buf;
+	const configuration _cfg;
+	cs::connected_socket _fd;
+	cs::input_stream<char> _read_buf;
+	cs::output_stream<char> _write_buf;
+	utils::message _read_msg;
+	utils::message _write_msg;
 	statistics _stats;
 };
 
 
 class client {
 public:
-	seastar::future<> start(bpo::variables_map config){
-		_server_addr = seastar::ipv4_addr{config["server"].as<std::string>()};
-		_concurrent_connections = seastar::smp::count * config["connections"].as<uint16_t>();
-		_requests_per_connection = config["requests_per_connection"].as<uint32_t>();
+	cs::future<> start(bpo::variables_map config){
+		_shard_id = cs::this_shard_id();
+		_server_addr = cs::ipv4_addr{config["server"].as<std::string>()};
+		_working_dir = config["working_dir"].as<std::filesystem::path>();
+		auto _connections = cs::smp::count * config["connections"].as<uint16_t>();
+
+		_common_cfg.n_msgs = config["msgs_per_connection"].as<uint32_t>();
+		_common_cfg.sleep_between_msgs = config["sleep_between_msgs"].as<bool>();
 
 		lg.debug("client issue connect_and_talk");
 
-		auto loop = seastar::parallel_for_each( boost::counting_iterator<uint16_t>(0)
-												, boost::counting_iterator<uint16_t>(_concurrent_connections)
-												, [this](size_t idx){ return connect_and_talk(idx);});
-		return loop.finally([this]{ return report_stats(); });
+		auto loop = cs::parallel_for_each( boost::counting_iterator<uint16_t>(0)
+										   , boost::counting_iterator<uint16_t>(_connections)
+										   , [this](size_t idx){ return connect_and_talk(idx);});
+
+		return loop.then([this]{
+			auto dump = [this](const auto& idx2stats){ return dump_stats(idx2stats.first, idx2stats.second); };
+			return cs::parallel_for_each(_stats.begin(), _stats.end(), dump);
+		});
     }
 
-    seastar::future<> stop() {
-        return seastar::make_ready_future();
+    cs::future<> stop() {
+        return cs::make_ready_future();
     }
 
 private:
-	seastar::future<> connect_and_talk(size_t idx){
-		seastar::socket_address local = seastar::socket_address(::sockaddr_in{AF_INET, INADDR_ANY, {0}});
-		auto socket = seastar::connect(make_ipv4_address(_server_addr), local, seastar::transport::TCP);
-		return socket.then([this, idx] (seastar::connected_socket fd) {
+	cs::future<> connect_and_talk(size_t idx){
+		cs::socket_address local = cs::socket_address(::sockaddr_in{AF_INET, INADDR_ANY, {0}});
+		auto socket = cs::connect(_server_addr, local, cs::transport::TCP);
+		return socket.then([this, idx] (cs::connected_socket fd) {
 				lg.debug("client connect_and_talk - connected({})", idx);
-                auto conn = seastar::make_lw_shared<connection>(idx, std::move(fd));
-				auto sent = conn->send(_requests_per_connection);
+                auto conn = cs::make_lw_shared<connection>(_common_cfg.change_id(idx), std::move(fd));
+				auto sent = conn->send();
 				return sent.then_wrapped([this, conn, idx] (auto&& f) {
 					try {
 						f.get();
-						lg.debug("client connect_and_talk - connection({}) sent all data(ok)", idx);
+						lg.info("client connect_and_talk - connection({}) sent all data(ok)", idx);
 						_stats[idx] = conn->stats();
 					} catch (std::exception& ex) {
 						lg.error("client connect_and_talk - connection({}) sent all data(error={})", idx, ex.what());
 					}
-					return seastar::make_ready_future<>();
+					return cs::make_ready_future<>();
                 });
         });
 	}
 
-	seastar::future<> report_stats() {
-		statistics total;
-		for(const auto& [idx, stats] : _stats){
-			std::cout << "idx=" << idx << " " << stats << std::endl;
-			total += stats;
-		}
+	cs::future<> dump_stats(const size_t idx, const statistics& conn_stats){
+		auto general_fname = _working_dir / cs::format("client_{}_conn_{}_general.csv", _shard_id, idx).c_str();
+		auto latency_fname = _working_dir / cs::format("client_{}_conn_{}_latency.csv", _shard_id, idx).c_str();
 
-		std::cout << "total " << total << std::endl;
-		return seastar::make_ready_future<>();
-    }
+		auto general = utils::write_file(general_fname, conn_stats.format_general());
+		auto latency = utils::write_file(latency_fname, conn_stats.format_latency());
+		return cs::when_all_succeed(std::move(general), std::move(latency))
+			   .then_wrapped([](auto unused){
+					return cs::make_ready_future<>();
+			   });
+	}
 
 private:
-    uint16_t _concurrent_connections;
-	int64_t _requests_per_connection;
-    seastar::ipv4_addr _server_addr;
-	seastar::lowres_clock::time_point _earliest_started;
-    seastar::lowres_clock::time_point _latest_finished;
-    unsigned _num_reported;
+	cs::shard_id _shard_id;
+    cs::ipv4_addr _server_addr;
+	connection::configuration _common_cfg;
+	std::filesystem::path _working_dir;
 	std::map<size_t, statistics> _stats;
 };
 
 
 int main(int ac, char ** av) {
-    seastar::app_template app;
+    cs::app_template app;
     app.add_options()
-        ("server", bpo::value<std::string>()->required(), "server address")
-        ("connections", bpo::value<uint16_t>()->default_value(16), "number of connections per cpu")
-        ("requests_per_connection", bpo::value<uint32_t>()->default_value(10000), "requests per connection")
+		//small numbers and default options for development, invocation scripts will be verbose
+        ("server", bpo::value<std::string>()->default_value(defaults::server_address), "server address")
+        ("connections", bpo::value<uint16_t>()->default_value(8), "number of connections per cpu")
+        ("msgs_per_connection", bpo::value<uint32_t>()->default_value(1000), "messages per connection")
+        ("sleep_between_msgs", bpo::value<bool>()->default_value(false), "sleep random period (us) between sent messages")
+		("working_dir", bpo::value<std::filesystem::path>()->default_value(std::filesystem::path(utils::current_time())), "the directory to store test relevant information");
         ;
 
-    return app.run(ac, av, [&app]{
-		auto& config = app.configuration();
+	std::stringstream arguments;
+	std::copy(av, av+ac, std::ostream_iterator<std::string>(arguments, " "));
 
-		auto started = clients.start().then([&config] () {
-			return clients.invoke_on_all(seastar::smp_submit_to_options(), &client::start, config);
+    return app.run(ac, av, [&app, &arguments]{
+
+		auto& config = app.configuration();
+		std::filesystem::path working_dir = config["working_dir"].as<std::filesystem::path>();
+
+		std::srand(std::time(nullptr));
+
+		auto touch_wdir = cs::recursive_touch_directory(working_dir.c_str());
+		auto saved_args = touch_wdir.then([working_dir, &arguments]{ return utils::write_file(working_dir / "args.txt", arguments.str()); });
+
+		return saved_args.then([&config]{
+
+			auto started = clients.start().then([&config] () {
+				return clients.invoke_on_all(cs::smp_submit_to_options(), &client::start, config);
+			});
+			return started.then([]{return clients.stop();});
 		});
-		return started.then([]{return clients.stop();});
 	});
 }
-//strace -s 1024 -o client.txt ./client --server 127.0.0.1:10000 -m 20M -c 1 --cpuset 1 --requests_per_connection=100 --connections 4 --default-log-level trace
+
+//clang++ -o client -ggdb client.cpp $(pkg-config --libs --cflags --static ~/development/seastar/build/debug/seastar.pc)
+//./client --memory 20M --smp 1 --cpuset 1
+//./client --memory 20M --smp 1 --cpuset 1 --default-log-level trace --server 127.0.0.1:10000 --msgs_per_connection=100 --connections 4 
--- server.cpp
+++ server.cpp
@@ -1,43 +1,53 @@
+#include "seastar/core/pipe.hh"
 #include "seastar/core/seastar.hh"
 #include "seastar/core/reactor.hh"
 #include "seastar/core/future-util.hh"
 #include "seastar/core/app-template.hh"
 #include "boost/program_options.hpp"
+#include "common.h"
 
 
-seastar::future<> handle_connection(seastar::accept_result accepted) {
-  auto out = accepted.connection.output();
-  auto in = accepted.connection.input();
-  return do_with( std::move(accepted), std::move(out), std::move(in),
-      [](auto &s, auto &out, auto &in) {
-        return seastar::repeat([&out, &in] {
-                 return in.read_exactly(4).then([&out](auto buf) {
-		   int x;
-		   x = 1;
-		   x++;
-                   if (buf) {
-                     return out.write(std::move(buf))
-                         .then([&out] { return out.flush(); })
-                         .then([] { return seastar::stop_iteration::no; });
-                   } else {
-                     return seastar::make_ready_future<seastar::stop_iteration>(seastar::stop_iteration::yes);
-                   }
-                 });
-               })
-            .then([&out] { return out.close(); });
-      });
+namespace cs = seastar;
+
+cs::logger lg("server");
+
+cs::future<> handle_connection(cs::accept_result accepted) {
+	auto out = accepted.connection.output();
+	auto in = accepted.connection.input();
+	auto done = do_with(std::move(accepted), std::move(out), std::move(in), utils::message{},
+    			   [](auto &s, auto &out, auto &in, auto& msg) {
+        return cs::repeat([&out, &in, &msg] {
+				auto read = utils::read_message(in, msg);
+				auto echo = read.then([&out, &msg]{ return utils::write_message(out, msg); });
+				return echo.then([]{ return cs::make_ready_future<cs::stop_iteration>(cs::stop_iteration::no); });
+	   	});
+    });
+
+	auto remote_address = accepted.remote_address;
+	return done.handle_exception([remote_address](std::exception_ptr err){
+		try{
+			std::rethrow_exception(err);
+		}
+		catch(cs::broken_pipe_exception& err){
+			lg.info("connection closed: {} - {}", remote_address, err.what());
+		} catch(std::exception& err){
+			lg.error("error: {} -{}", remote_address, err.what());
+		}
+		return cs::make_ready_future<>();
+	});
 }
 
 
-seastar::future<> service_loop(uint16_t port) {
-	const seastar::listen_options lo = {.reuse_address = true};
-	return seastar::do_with(seastar::listen(seastar::make_ipv4_address({port}), lo), [](auto &listener) {
-			return seastar::keep_doing([&listener]() {
+cs::future<> service_loop(uint16_t port) {
+	const cs::listen_options lo = {.reuse_address = true};
+	return cs::do_with(cs::listen(cs::make_ipv4_address({port}), lo), [](auto &listener) {
+			return cs::keep_doing([&listener]() {
 				return listener.accept().then(
-					[](seastar::accept_result accepted) {
+					[](cs::accept_result accepted) {
 					// Note we ignore, not return, the future returned by
 					// handle_connection(), so we do not wait for one
 					// connection to be handled before accepting the next one.
+					lg.info("connection accepted - {}", accepted.remote_address);
 					auto done = handle_connection(std::move(accepted));
 				});
 			});
@@ -45,33 +55,30 @@ seastar::future<> service_loop(uint16_t port) {
 }
 
 
-seastar::future<> start_service_loop_on_core(seastar::app_template& app, uint16_t core){
+cs::future<> start_service_loop_on_core(cs::app_template& app, uint16_t core){
 	auto& args = app.configuration();
 	uint16_t port = args["port"].as<uint16_t>();
 	if (args["port_per_core"].as<bool>())
 		port += core;
-	return seastar::smp::submit_to(core, [port](){ return service_loop(port); });
+	return cs::smp::submit_to(core, [port](){ return service_loop(port); });
 }
 
 
-seastar::future<> start_service_loop(seastar::app_template& app) {
-	seastar::engine().at_exit([](){::_exit(0); return seastar::make_ready_future<>(); }); //This is Sparta!!!
-
-	auto smp_submit = [&app](uint16_t core){ return start_service_loop_on_core(app, core); };
-
-
-	return seastar::parallel_for_each(boost::irange<unsigned>(0, seastar::smp::count), smp_submit);
+cs::future<> start_service_loop(cs::app_template& app) {
+	cs::engine().at_exit([](){::_exit(0); return cs::make_ready_future<>(); }); //This is Sparta!!!
+	auto smp_submit = [&app](uint16_t core){ return start_service_loop_on_core(app, core); }; 
+	return cs::parallel_for_each(boost::irange<unsigned>(0, cs::smp::count), smp_submit);
 }
 
 
 int main(int argc, char** argv){
 	namespace bpo = boost::program_options;
-	seastar::app_template app;
+	cs::app_template app;
     app.add_options()
-        ("port", bpo::value<uint16_t>()->default_value(10000), "server port")
+        ("port", bpo::value<uint16_t>()->default_value(defaults::server_port), "server port")
         ("port_per_core", bpo::value<bool>()->default_value(true), "use unique port per core");
 	return app.run(argc, argv, [&app](){return start_service_loop(app);});
 }
 
-//clang++ -o server -ggdb server.cpp $(pkg-config --libs --cflags --static ~/devel/seastar/build/debug/seastar.pc)
+//clang++ -o server -ggdb server.cpp $(pkg-config --libs --cflags --static ~/development/seastar/build/debug/seastar.pc)
 
